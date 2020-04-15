c3153232ec5700be43a0d406372e0c276546c5fd
Hieu Minh Le <hieu.minhle@nashtechglobal.com>
2020-04-16 02:38:27 +0700

    Initial commit.

---
+++ src/main/java/io/hieu/coronavirustracker/models/LocationStats.java
@@ -0,0 +1,49 @@
+package io.hieu.coronavirustracker.models;
+
+public class LocationStats {
+    private String state;
+    private String country;
+    private int latestTotalCases;
+    private int differencesFromPreviousDay;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getLatestTotalCases() {
+        return latestTotalCases;
+    }
+
+    public int getDifferencesFromPreviousDay() {
+        return differencesFromPreviousDay;
+    }
+
+    public void setDifferencesFromPreviousDay(int differencesFromPreviousDay) {
+        this.differencesFromPreviousDay = differencesFromPreviousDay;
+    }
+
+    public void setLatestTotalCases(int latestTotalCases) {
+        this.latestTotalCases = latestTotalCases;
+    }
+
+    @Override
+    public String toString() {
+        return "LocationStats{" +
+                "state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", latestTotalCases=" + latestTotalCases +
+                '}';
+    }
+}
